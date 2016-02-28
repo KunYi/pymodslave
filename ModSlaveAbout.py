c1fa18657d7823b8ddaec7f7dac636e57aa6e9e9
eleftherios barbas <eleftherios.barbas@gmail.com>
2016-02-28 12:11:15 +0200

    Fix UI.

--- ModSlaveAbout.py
+++ ModSlaveAbout.py
@@ -13,7 +13,7 @@
 from PyQt4 import QtGui,QtCore
 from Ui_about import Ui_About
 
-_VERSION = "0.3.7"
+_VERSION = "0.3.8"
 _URL = "<a href = ""http://sourceforge.net/projects/pymodslave"">Sourceforge Project Home Page</a>"
 
 #-------------------------------------------------------------------------------
