400bc78f4f5d6e2352c2efa747a714938e15586a
ElBar <ElBar@localhost>
2019-03-11 21:46:00 +0200

    Prepare uploading to pypi.org

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyModSlave",
+    version="0.4.2-1",
+    author="ElBar",
+    author_email="eleftherios.barbas@gmail.com",
+    description="A modbus RTU/TCP slave application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://sourceforge.net/projects/pymodslave/",
+    packages=setuptools.find_packages(),
+	packages_data={
+	'pyModSlave':['ManModbus:*']},
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
